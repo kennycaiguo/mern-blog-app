a0d161f0dd72ac3c4e6891d61347b826f3dffa91
kennycaiguo <kennycai8888@outlook.com>
2025-03-27 17:23:48 -0600

    connect to database

--- api/index.js
+++ api/index.js
@@ -1,5 +1,15 @@
 import express from 'express'
+import mongoose from 'mongoose'
+import dotenv from 'dotenv'
 
+dotenv.config() //读取配置文件
+mongoose.connect(process.env.MONGO)
+         .then(()=>{
+            console.log("MongoDB is connected");
+         })
+         .catch(err=>{
+            console.log(err);
+         })
 let app = express()
 let port = 3000
 
