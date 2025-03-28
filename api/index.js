0e3c44802248679653b46e386a28a024b4584c16
kennycaiguo <kennycai8888@outlook.com>
2025-03-27 18:38:33 -0600

    created a test route

--- api/index.js
+++ api/index.js
@@ -1,6 +1,7 @@
 import express from 'express'
 import mongoose from 'mongoose'
 import dotenv from 'dotenv'
+import userRoute from './routes/user.routes.js'
 
 dotenv.config() //读取配置文件
 mongoose.connect(process.env.MONGO)
@@ -16,3 +17,6 @@ let port = 3000
 app.listen(port,()=>{
     console.log(`server is running: http://localhost:${port}/ !!!`);
 })
+
+//使用我们导入的userRoute
+app.use("/api/user",userRoute)
---
+++ api/routes/user.routes.js
@@ -0,0 +1,8 @@
+import express from 'express'
+import {test}  from '../controllers/user.controller.js'
+
+let router = express.Router()
+
+router.get("/test",test)
+
+ export default router
